c1c72084360a8cfbfefe4ae2d7f96a365a4022d0
Peter <51321635+peterbodizs@users.noreply.github.com>
2021-12-01 14:47:18 +0100

    day1a

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, Advent of Code 2021!");
+
+var lines = File.ReadAllLines("Inputs/Day1a.txt");
+var count = 0;
+
+for (int i = 1; i < lines.Length; i++)
+{
+    var previousDepth = int.Parse(lines[i-1]);
+    var currentDepth = int.Parse(lines[i]);
+
+    if (previousDepth < currentDepth) count++;
+}
+
+Console.WriteLine(count);
