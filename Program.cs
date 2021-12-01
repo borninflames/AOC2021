91ecef1d0b24526b594c3a1ba959ab2c74c538e9
Peter <51321635+peterbodizs@users.noreply.github.com>
2021-12-01 14:55:43 +0100

    Day1b

--- Program.cs
+++ Program.cs
@@ -2,12 +2,22 @@
 Console.WriteLine("Hello, Advent of Code 2021!");
 
 var lines = File.ReadAllLines("Inputs/Day1a.txt");
-var count = 0;
+var measurements = new List<int>();
 
-for (int i = 1; i < lines.Length; i++)
+for (int i = 2; i < lines.Length; i++)
 {
-    var previousDepth = int.Parse(lines[i-1]);
-    var currentDepth = int.Parse(lines[i]);
+    var depth1 = int.Parse(lines[i-2]);
+    var depth2 = int.Parse(lines[i - 1]);
+    var depth3 = int.Parse(lines[i]);
+    measurements.Add(depth1 + depth2 + depth3);
+}
+
+
+var count = 0;
+for (int i = 1; i < measurements.Count; i++)
+{
+    var previousDepth = measurements[i - 1];
+    var currentDepth = measurements[i];
 
     if (previousDepth < currentDepth) count++;
 }
