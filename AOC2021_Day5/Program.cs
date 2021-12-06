11b0130609f41760974bb3a2b00a3df92cbb5e70
Peter <51321635+peterbodizs@users.noreply.github.com>
2021-12-06 20:44:04 +0100

    Day 5 part 2

---
+++ AOC2021_Day5/Program.cs
@@ -0,0 +1,78 @@
+﻿Console.WriteLine("Hello, Advent of Code 2021!");
+
+var lines = File.ReadAllLines("Input.txt");
+var space = new Space();
+foreach (var line in lines)
+{
+    space.AddLine(line);
+}
+
+Console.WriteLine(space.Points.Where(p => p.Intersects > 1).Count());
+
+
+class Point
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public int Intersects { get; set; } = 1;
+
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public Point(string xy)
+    {
+        var coordsStr = xy.Split(',');
+        X = Convert.ToInt32(coordsStr[0]);
+        Y = Convert.ToInt32(coordsStr[1]);
+    }
+    public override bool Equals(object? p)
+    {
+        var result = p is Point point && X == point.X && Y == point.Y;
+        return result;
+    }
+
+    public override string ToString()
+    {
+        return $"X: {X}; Y: {Y}; I: {Intersects}";
+    }
+}
+
+class Space
+{
+    public List<Point> Points { get; set; } = new List<Point>();
+
+    public void AddLine(string line)
+    {
+        var endPointsStr = line.Split(" -> ");
+        var p1 = new Point(endPointsStr[0]);
+        var p2 = new Point(endPointsStr[1]);
+
+        var xDirection = p1.X > p2.X ? -1 : p1.X == p2.X ? 0 : 1;
+        var yDirection = p1.Y > p2.Y ? -1 : p1.Y == p2.Y ? 0 : 1;
+                
+        AddOrUpdatePoint(p1);
+        do
+        {           
+            p1 = new Point(p1.X + xDirection, p1.Y + yDirection);
+            AddOrUpdatePoint(p1);
+
+        } while (!p1.Equals(p2));
+    }
+
+    private void AddOrUpdatePoint(Point p)
+    {
+        var point = Points.FirstOrDefault(pt => pt.Equals(p));
+        if (point != null)
+        {
+            point.Intersects++;
+        }
+        else
+        {
+            Points.Add(p);
+        }
+    }
+}
