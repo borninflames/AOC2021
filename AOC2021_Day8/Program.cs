133e6bdeb6fe3441184b8a6f33d0dd4ddc023743
Peter <51321635+peterbodizs@users.noreply.github.com>
2021-12-10 10:23:12 +0100

    Day9 part 1

--- AOC2021_Day8/Program.cs
+++ AOC2021_Day8/Program.cs
@@ -19,3 +19,69 @@ foreach (var line in lines)
 }
 
 Console.WriteLine(sum);
+var outputSum = 0;
+foreach (var line in lines)
+{
+    var inputOutput = line.Split(" | ");
+    var iDigits = inputOutput[0].Split(' ').OrderBy(id => id.Length == 5 ? int.MaxValue : id.Length).ToArray();
+    var oDigits = inputOutput[1].Split(' ').Select(od => string.Concat(od.OrderBy(c => c))).Reverse().ToArray();
+    var mapping = new Dictionary<int, string>();
+    char upperHalfOfOne = 'x';
+    foreach (var digit in iDigits)
+    {
+        var num = -1;
+        switch (digit.Length)
+        {
+            case 2:
+                num = 1;
+                break;
+            case 3:
+                num = 7;
+                break;
+            case 4:
+                num = 4;
+                break;
+            case 5:
+                if (mapping[7].All(c => digit.Contains(c)))
+                {
+                    num = 3;
+                }
+                else if (digit.Contains(upperHalfOfOne))
+                {
+                    num = 2;
+                }
+                else
+                {
+                    num = 5;
+                }
+                break;
+            case 6:
+                if (mapping[4].All(c => digit.Contains(c)))
+                {
+                    num = 9;
+                }
+                else if (mapping[1].All(c => digit.Contains(c)))
+                {
+                    num = 0;
+                }
+                else
+                {
+                    num = 6;
+                    upperHalfOfOne = digit.Contains(mapping[1][1]) ? mapping[1][0] : mapping[1][1];
+                }
+                break;
+            case 7:
+                num = 8;
+                break;
+        }
+        mapping[num] = string.Concat(digit.OrderBy(c => c));
+    }
+
+    for (int i = 0; i < oDigits.Length; i++)
+    {
+        var num = mapping.First(m => m.Value == oDigits[i]).Key;
+        outputSum += num * (int)Math.Pow(10, i);
+    }
+}
+
+Console.WriteLine(outputSum);
---
+++ AOC2021_Day9/Program.cs
@@ -0,0 +1,74 @@
+﻿Console.WriteLine("Hello, Advent of Code 2021!");
+
+//var lines = File.ReadAllLines("TestInput.txt");
+var lines = File.ReadAllLines("Input.txt");
+
+var width = lines[0].Length + 2;
+var height = lines.Length + 2;
+
+var depthMatrix = new int[height, width];
+
+for (int i = 0; i < width; i++)
+{
+    depthMatrix[0, i] = int.MaxValue;
+    depthMatrix[height - 1, i] = int.MaxValue;
+}
+
+for (int i = 0; i < height; i++)
+{
+    depthMatrix[i, 0] = int.MaxValue;
+    depthMatrix[i, width - 1] = int.MaxValue;
+}
+
+for (int row = 0; row < lines.Length; row++)
+{
+    for (int col = 0; col < lines[0].Length; col++)
+    {
+        depthMatrix[row+1, col+1] = int.Parse(lines[row][col].ToString());        
+    }
+}
+
+var sum = 0;
+
+for (int row = 1; row <= lines.Length; row++)
+{
+    for (int col = 1; col <= lines[0].Length; col++)
+    {
+        if (IsLowest(depthMatrix, row, col))
+        {
+            sum += depthMatrix[row, col]+1;
+            Console.WriteLine(depthMatrix[row, col]);
+        }
+    }
+}
+
+WriteOutMatrix(depthMatrix);
+
+Console.WriteLine(sum);
+
+bool IsLowest(int[,] m, int row, int col)
+{
+    return m[row, col] < m[row - 1, col] && m[row, col] < m[row + 1, col] &&
+        m[row, col] < m[row, col-1] && m[row, col] < m[row, col+1];
+}
+
+
+void WriteOutMatrix(int[,] depthMatrix)
+{
+    for (int row = 0; row < height; row++)
+    {
+        for (int col = 0; col < width; col++)
+        {
+            if (depthMatrix[row, col] == int.MaxValue)
+            {
+                Console.Write("X");
+            }
+            else
+            {
+                Console.Write(depthMatrix[row, col]);
+            }
+
+        }
+        Console.WriteLine();
+    }
+}
