a4bcfc97b71c9f7eb5d085bb17d47b6b2411dd84
Peter <51321635+peterbodizs@users.noreply.github.com>
2021-12-10 19:50:09 +0100

    Day 10 part 1

---
+++ AOC2021_Day_10/Program.cs
@@ -0,0 +1,64 @@
+﻿Console.WriteLine("Hello, Advent of Code 2021!");
+
+//var lines = File.ReadAllLines("TestInput.txt");
+var lines = File.ReadAllLines("Input.txt");
+
+var brackets = new Dictionary<char, char>
+{
+    { '[', ']' },
+    { '(', ')' },
+    { '{', '}' },
+    { '<', '>' }
+};
+
+var characterScore = new Dictionary<char, int>
+{
+    { ')', 3 },
+    { ']', 57 },
+    { '}', 1197 },
+    { '>', 25137 }
+};
+
+var sum = 0;
+
+foreach (var line in lines)
+{
+    var stack = new Stack<char>();
+    Console.WriteLine(IsValid(line, stack));
+}
+
+Console.WriteLine();
+Console.WriteLine($"The answer is: {sum}");
+
+bool IsValid(string chunks, Stack<char> stack)
+{
+    if (chunks.Length == 0)
+    {
+        return true;
+    }
+
+    var ch = chunks[0];
+    var subString = chunks.Substring(1);
+
+    if (brackets.ContainsKey(ch))
+    {
+        stack.Push(ch);
+        return IsValid(subString, stack);
+    }
+
+    if (stack.Count == 0)
+    {
+        return false;
+    }
+
+    var lastOpen = stack.Pop();
+
+    if (brackets[lastOpen] != ch)
+    {
+        Console.WriteLine($"Illegal character found: {ch}");
+        sum += characterScore[ch];
+        return false;
+    }
+
+    return IsValid(subString, stack);
+}
