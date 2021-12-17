f03cd65b3a9ba31848c22f4750752e9c6eca8ea7
Peter <51321635+peterbodizs@users.noreply.github.com>
2021-12-17 22:02:55 +0100

    Day 15 part 1 finalyl

---
+++ AOC2021_Day_15/Program.cs
@@ -0,0 +1,244 @@
+﻿using System.Drawing;
+
+namespace AOC2021_Day_15
+{
+    class Program
+    {
+        static int rows = 0;
+        static int cols = 0;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Advent of Code 2021!");
+
+            //var lines = File.ReadAllLines("TestInput.txt");
+            var lines = File.ReadAllLines("Input.txt");
+
+            rows = lines.Length;
+            cols = lines[0].Length;
+
+            var cavern = new RiskPoint[rows, cols];
+            var caveSystem = new Dictionary<Point, RiskPoint>();
+
+            for (int r = 0; r < lines.Length; r++)
+            {
+                for (int c = 0; c < lines[r].Length; c++)
+                {
+                    //cavern[r, c] = new RiskPoint(Convert.ToInt32(lines[r][c].ToString()), r, c);
+                    //caveSystem.Add(new Point(c, r), new RiskPoint(Convert.ToInt32(lines[r][c].ToString()), r, c));
+                    caveSystem[new Point(c, r)] = new RiskPoint(Convert.ToInt32(lines[r][c].ToString()), r, c);
+                    if (r == 0 && c == 0)
+                    {
+                        //cavern[r, c].RiskToReach = 0;
+                        caveSystem[new Point(0, 0)].RiskToReach = 0;
+                    }
+                }
+            }
+
+            Console.CursorVisible = false;
+
+            //Traverse(cavern, cavern[0, 0]);
+            Dijkstra(caveSystem, caveSystem[new Point(0, 0)]);
+
+            //ShowMatrix(cavern);
+
+            Console.CursorVisible = true;
+
+            Console.WriteLine();
+            Console.WriteLine(caveSystem[new Point(cols - 1, rows - 1)].RiskToReach);
+        }
+
+        public static void Dijkstra(Dictionary<Point, RiskPoint> caveSystem, RiskPoint startingPoint)
+        {
+            var cavesToCheck = new List<RiskPoint>();
+            cavesToCheck.Add(startingPoint);
+            //startingPoint.IsVisited = true;
+            while (cavesToCheck.Any(c => !c.IsVisited))
+            {
+                cavesToCheck = cavesToCheck.Where(c => !c.IsVisited).OrderBy(c => c.RiskToReach).ToList();
+                var currentCave = cavesToCheck.First();
+                cavesToCheck.RemoveAt(0);
+                currentCave.IsVisited = true;
+                //ShowMatrix(cavern);
+
+                if (currentCave.Col < cols - 1 && !caveSystem[new Point(currentCave.Col + 1, currentCave.Row)].IsVisited)
+                {
+                    var nextCave = caveSystem[new Point(currentCave.Col + 1, currentCave.Row)];
+                    cavesToCheck.Add(nextCave);
+                    SetLowestRisk(currentCave, nextCave);
+                }
+
+                if (currentCave.Row < rows - 1 && !caveSystem[new Point(currentCave.Col, currentCave.Row + 1)].IsVisited)
+                {
+                    var nextCave = caveSystem[new Point(currentCave.Col, currentCave.Row + 1)];
+                    cavesToCheck.Add(nextCave);
+                    SetLowestRisk(currentCave, nextCave);
+                }
+
+                if (currentCave.Col > 0 && !caveSystem[new Point(currentCave.Col - 1, currentCave.Row)].IsVisited)
+                {
+                    var nextCave = caveSystem[new Point(currentCave.Col - 1, currentCave.Row)];
+                    cavesToCheck.Add(nextCave);
+                    SetLowestRisk(currentCave, nextCave);
+                }
+
+                if (currentCave.Row > 0 && !caveSystem[new Point(currentCave.Col, currentCave.Row - 1)].IsVisited)
+                {
+                    var nextCave = caveSystem[new Point(currentCave.Col, currentCave.Row - 1)];
+                    cavesToCheck.Add(nextCave);
+                    SetLowestRisk(currentCave, nextCave);
+                }
+            }
+        }
+
+        public static void FindPath(RiskPoint[,] cavern, RiskPoint startingPoint)
+        {
+            //var cavesToCheck = new List<Point>();
+            //cavesToCheck.Add(startingPoint);
+            startingPoint.IsVisited = true;
+            var visitedcount = 0;
+
+            while (visitedcount < rows * cols)
+            {
+                //cavesToCheck = cavesToCheck.OrderBy(c => c.RiskToReach).ToList();
+                var currentCave = cavern[0, 0]; //cavesToCheck.First();
+                //cavesToCheck.RemoveAt(0);
+
+                visitedcount++;
+
+                //ShowMatrix(cavern);
+
+                if (currentCave.Col < cols - 1 && !cavern[currentCave.Row, currentCave.Col + 1].IsVisited)
+                {
+                    var nextCave = cavern[currentCave.Row, currentCave.Col + 1];
+                    //cavesToCheck.Add(nextCave);
+                    SetLowestRisk(currentCave, nextCave);
+                    //nextCave.IsVisited = true;
+                }
+
+                if (currentCave.Row < rows - 1 && !cavern[currentCave.Row + 1, currentCave.Col].IsVisited)
+                {
+                    var nextCave = cavern[currentCave.Row + 1, currentCave.Col];
+                    //cavesToCheck.Add(nextCave);
+                    SetLowestRisk(currentCave, nextCave);
+                    //nextCave.IsVisited = true;
+                }
+
+                if (currentCave.Col > 0 && !cavern[currentCave.Row, currentCave.Col - 1].IsVisited)
+                {
+                    var nextCave = cavern[currentCave.Row, currentCave.Col - 1];
+                    //cavesToCheck.Add(nextCave);
+                    SetLowestRisk(currentCave, nextCave);
+                    //nextCave.IsVisited = true;
+                }
+
+                if (currentCave.Row > 0 && !cavern[currentCave.Row - 1, currentCave.Col].IsVisited)
+                {
+                    var nextCave = cavern[currentCave.Row - 1, currentCave.Col];
+                    //cavesToCheck.Add(nextCave);
+                    SetLowestRisk(currentCave, nextCave);
+                    //nextCave.IsVisited = true;
+                }
+
+                currentCave.IsVisited = true;
+            }
+        }
+
+        public static void Traverse(RiskPoint[,] cavern, RiskPoint currentCave)
+        {
+            if (currentCave.IsVisited)
+            {
+                return;
+            }
+
+            currentCave.IsVisited = true;
+
+            ShowMatrix(cavern);
+            var cavesToVisit = new List<RiskPoint>();
+            if (currentCave.Col < cols - 1 && !cavern[currentCave.Row, currentCave.Col + 1].IsVisited)
+            {
+                var nextCave = cavern[currentCave.Row, currentCave.Col + 1];
+                cavesToVisit.Add(nextCave);
+                SetLowestRisk(currentCave, nextCave);
+            }
+
+            if (currentCave.Row < rows - 1 && !cavern[currentCave.Row + 1, currentCave.Col].IsVisited)
+            {
+                var nextCave = cavern[currentCave.Row + 1, currentCave.Col];
+                cavesToVisit.Add(nextCave);
+                SetLowestRisk(currentCave, nextCave);
+            }
+
+            if (currentCave.Col > 0 && !cavern[currentCave.Row, currentCave.Col - 1].IsVisited)
+            {
+                var nextCave = cavern[currentCave.Row, currentCave.Col - 1];
+                cavesToVisit.Add(nextCave);
+                SetLowestRisk(currentCave, nextCave);
+            }
+
+            if (currentCave.Row > 0 && !cavern[currentCave.Row - 1, currentCave.Col].IsVisited)
+            {
+                var nextCave = cavern[currentCave.Row - 1, currentCave.Col];
+                cavesToVisit.Add(nextCave);
+                SetLowestRisk(currentCave, nextCave);
+            }
+
+            cavesToVisit = cavesToVisit.OrderBy(c => c.RiskToReach).ToList();
+            foreach (var cave in cavesToVisit)
+            {
+                Traverse(cavern, cave);
+            }
+        }
+
+        public static void ShowMatrix(RiskPoint[,] cavern)
+        {
+            for (int r = 0; r < cavern.GetLength(0); r++)
+            {
+                for (int c = 0; c < cavern.GetLength(1); c++)
+                {
+                    Console.SetCursorPosition(c, r + 1);
+                    if (cavern[r, c].IsVisited)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                    }
+
+                    Console.Write(cavern[r, c].Risk);
+                }
+            }
+        }
+
+        private static void SetLowestRisk(RiskPoint currentCave, RiskPoint cave)
+        {
+            if (cave.RiskToReach > cave.Risk + currentCave.RiskToReach)
+            {
+                cave.RiskToReach = cave.Risk + currentCave.RiskToReach;
+            }
+        }
+    }
+
+    class RiskPoint
+    {
+        public RiskPoint(int risk, int row, int col)
+        {
+            Risk = risk;
+            Row = row;
+            Col = col;
+        }
+
+        public int Risk { get; set; }
+        public int Row { get; set; }
+        public int Col { get; set; }
+
+        public bool IsVisited { get; set; }
+        public int RiskToReach { get; set; } = int.MaxValue;
+
+        public override string ToString()
+        {
+            return $"[{Row}, {Col}] <<{RiskToReach}>> | {Risk}";
+        }
+    }
+}
--- AOC_Day_12/Program.cs
+++ AOC_Day_12/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("Hello, Advent of Code 2021!");
 
-//var lines = File.ReadAllLines("TestInput.txt");
-var lines = File.ReadAllLines("Input.txt");
+var lines = File.ReadAllLines("TestInput.txt");
+//var lines = File.ReadAllLines("Input.txt");
 var caveSystem = new Dictionary<string, Cave>();
 
 foreach (var line in lines)
@@ -29,7 +29,12 @@ Console.ReadLine();
 
 void Traverse(Dictionary<string, Cave> caveSystem, Cave cave, List<string> visitedCaves)
 {
-    if (cave.IsSmall && visitedCaves.Contains(cave.Name) )
+    //if ((cave.IsSingleSmall && visitedCaves.Where(c => c == cave.Name).Count() == 2) || (cave.IsSmall && visitedCaves.Contains(cave.Name) && !cave.IsSingleSmall))
+    //{
+    //    return;
+    //}
+    var visitedCount = visitedCaves.Where(c => c == cave.Name).Count();
+    if ((cave.IsSmall && visitedCount == 1 && !cave.IsSingleSmall()) || (cave.IsSingleSmall() && visitedCount == 2))
     {
         return;
     }
@@ -56,13 +61,21 @@ class Cave
     {
         Name = name;
         IsSmall = name.ToLower() == name;
+        IsBig = name.ToUpper() == name;
         IsEnd = name == "end";
+        IsStart = name == "start";
     }
 
     public string Name { get; set; }
 
     public bool IsSmall { get; set; }
+    public bool IsBig { get; set; }
+    public bool IsSingleSmall() 
+    {
+        return !IsStart && !IsEnd && !IsBig && Caves.Any(c => c.IsBig);
+    }
     public bool IsEnd { get; set; }
+    public bool IsStart { get; set; }
 
     public int VisitedTimes { get; set; } = 0;
 
